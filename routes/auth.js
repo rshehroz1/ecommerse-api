dfad037cb5cc2ceb21cdea1a403a6b32cc169970
Shehroz <116956168+rshehroz1@users.noreply.github.com>
2023-03-12 20:29:22 +0500

    xatolar tuzatildi parol hash qilindi

--- routes/auth.js
+++ routes/auth.js
@@ -40,4 +40,28 @@ router.post('/signup',(req, res)=>{
     })
 })
 
+router.post("/signin", (req, res) =>{
+    const { email, password } = req.body
+    if(!email || !password){
+        res.status(422).json({error: "Plase add email or password"})
+    }
+    User.findOne({email: email})
+    .then(savedUser =>{
+        if(!savedUser){
+            return res.status(422).json({error: "Invalid email or password"})
+        }
+        bcrypt.compare(password, savedUser.password)
+        .then(doMatch =>{
+            if(doMatch){
+                res.json({msg: "succesfully signed in"})
+            }else{
+                return res.status(422).json({error: "Invalid password"})
+            }
+        })
+        .catch(err =>{
+            console.log(err);
+        })
+    })
+})
+
 module.exports = router
