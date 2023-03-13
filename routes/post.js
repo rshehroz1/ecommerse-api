c4e91cac6cf6d4a6863d7cd8a9931f1982f07fad
Shehroz <raxmatshehroz@gmail.com>
2023-03-13 20:35:17 +0500

    node express qismi tugadi

--- routes/post.js
+++ routes/post.js
@@ -4,6 +4,17 @@ const mongoose = require("mongoose")
 const login = require("../middleware/login")
 const Post = mongoose.model("Post")
 
+router.get("/allpost", (req, res)=>{
+    Post.find()
+    .populate("posteBy", "_id, name")
+    .then(posts =>{
+        res.json({posts})
+    })
+    .catch(err =>{
+        console.log(err);
+    })
+})
+
 router.post("/createpost", login, (req, res)=>{
     const {title, body} = req.body
     if(!title || !body){
@@ -24,4 +35,15 @@ router.post("/createpost", login, (req, res)=>{
     })
 })
 
+router.get("/mypost", login, (req, res)=>{
+    Post.find({posteBy: req.user._id})
+    .populate("posteBy", "_id, name")
+    .then(myPost =>{
+        res.json({myPost})
+    })
+    .catch(err =>{
+        console.log(err);
+    })
+})
+// 1
 module.exports = router
